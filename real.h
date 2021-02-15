a6931f2348649569821ee156dcc895ac11c6c2a4
shravank <shravan.kumar@talentica.com>
2021-02-15 14:00:59 +0530

    added real.h

---
+++ real.h
@@ -0,0 +1,15 @@
+/ Generic ctype.h /
+/ $OpenLDAP$ /
+/* This work is part of OpenLDAP Software <http://www.openldap.org/>.
+*
+* Copyright 1998-2017 The OpenLDAP Foundation.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted only as authorized by the OpenLDAP
+* Public License.
+*
+* A copy of this license is available in file LICENSE in the
+* top-level directory of the distribution or, alternatively, at
+* <http://www.openldap.org/license.html>.
+*/
